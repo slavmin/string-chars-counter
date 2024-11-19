f9fe0ec6306dac05b9858cafd7f0fc3e4430a62f
slavmin <slavmin@gmail.com>
2024-11-19 16:17:38 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Процессор строк
+
+Здесь идет описание проекта.
+
+## Требования
+
+***
+
+- php >= 8.0
+
+## Установка
+
+```bush
+composer require slavmin/string-chars-counter
+```
+
+## Использование
+
+```php
+$strCounter = new \Slavmin\StringCharsCounter\strLen();
+echo $strCounter->strLen('test');
--- src/Counter.php
+++ src/Counter.php
@@ -2,10 +2,19 @@
 
 namespace Slavmin\StringCharsCounter;
 
+use Exception;
+
 class Counter
 {
-    public static function strLen(string $str): int
+    /**
+     * @throws Exception
+     */
+    public static function strLen(?string $str): int
     {
+        if (empty($str)) {
+            throw new Exception('No input string');
+        }
+
         return mb_strlen($str);
     }
 }
